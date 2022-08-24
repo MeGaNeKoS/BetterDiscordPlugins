453fff7ae597f9bdd2ddd72f94f2029b37bcb72c
めがねこ <29650686+MeGaNeKoS@users.noreply.github.com>
2022-08-25 04:08:26 +0700

    Update FakeMuteDeafen.plugin.js

--- FakeMuteDeafen/FakeMuteDeafen.plugin.js
+++ FakeMuteDeafen/FakeMuteDeafen.plugin.js
@@ -2,66 +2,55 @@
  * @name Fake Mutes Deafen
  * @author MeGaNeKo
  * @description I didn't hear anything... I swear
- * @version 0.0.1
+ * @version 0.0.2
  * @updateUrl https://raw.githubusercontent.com/MeGaNeKoS/BetterDiscordPlugins/main/FakeMuteDeafen/FakeMuteDeafen.plugin.js
  */
+
 module.exports = meta => {
     const mySettings = {
         mode: 0
     };
-    const decoder = new TextDecoder("utf-8");
+    const erlpack = DiscordNative.nativeModules.requireModule("discord_erlpack")
 
-    let last_channel = null // to avoid the discord reannounce status.
-
+	
     if (WebSocket.prototype.original == undefined) {
         // store the original WebSocket
         WebSocket.prototype.original = WebSocket.prototype.send
     }
 
     WebSocket.prototype.send_modded = function(data) {
-        if (Object.prototype.toString.call(data) === "[object ArrayBuffer]") {
-            if (decoder.decode(data)
-                .includes("guild_ids\u0003nil")) {
-                // in here we are disconnect from vc
-                last_channel = null
-            }
+        const dataRaw = data
+        try {
+            if (Object.prototype.toString.call(data) === "[object ArrayBuffer]") {
+                let packed = new Uint8Array(data)
+                let unpacked = erlpack.unpack(packed)
 
-            if (decoder.decode(data)
-                .includes(last_channel)) {
-                if (mySettings.mode == 0) {
-                    // stuck on deafen/mute status
-                    if (decoder.decode(data)
-                        .includes("self_mutes\u0005false") && decoder.decode(data)
-                        .includes("self_deafs\u0005false")) {
-                        return
-                    }
-                } else if (mySettings.mode == 1) {
-                    // stuck on deafen only
-                    if (decoder.decode(data)
-                        .includes("self_deafs\u0005false")) {
+                if (unpacked.op == 4) {
 
-                        return
+                    if (unpacked.d.channel_id != null &&
+                        unpacked.d.guild_id != null &&
+                        unpacked.d.preferred_region != null) {
+							
+                        let t = {
+                            "op": 4,
+                            "d": {
+                                "guild_id": unpacked.d.guild_id,
+                                "channel_id": unpacked.d.channel_id,
+                                "self_mute": mySettings.mode < 4 ? ["0", "1"].includes(mySettings.mode) : unpacked.d.self_mute, // mute deafen, mute only
+                                "self_deaf": mySettings.mode < 4 ? ["0", "2"].includes(mySettings.mode) : unpacked.d.self_deaf, // mute deafen, deafen only
+                                "self_video": unpacked.d.self_video,
+                                "preferred_region": unpacked.d.preferred_region
+                            }
+                        }
+                        data = erlpack.pack(t).buffer
                     }
-                } else if (mySettings.mode == 2) {
-                    // normal
                 }
             }
-            if (decoder.decode(data)
-                .includes("channel_idm\u0000\u0000\u0000")) {
-
-                // we detect a voice channel in the data
-                // store the new channel
-                start = decoder.decode(data)
-                    .split("channel_idm\u0000\u0000\u0000")[1]
-                end = start.split("\u0000\u0000\u0000	self_mutes")[0]
-                last_channel = "channel_idm\u0000\u0000\u0000" + end + "\u0000\u0000\u0000	self_mutes"
-            }
-
+            WebSocket.prototype.original.apply(this, [data]);
+        } catch (err) {
+            WebSocket.prototype.original.apply(this, [dataRaw]);
         }
-        WebSocket.prototype.original.apply(this, [data]);
     }
-
-
 
     function buildSetting(text, key, type = () => {}) {
         const setting = Object.assign(document.createElement("div"), {
@@ -76,18 +65,28 @@ module.exports = meta => {
         const option1 = document.createElement("option");
 
         option1.value = 0;
-        option1.text = "Mute/Deafen";
+        option1.text = "Mute + Deafen";
         input.appendChild(option1);
 
         const option2 = document.createElement("option");
         option2.value = 1;
-        option2.text = "Deafen";
+        option2.text = "Mute only";
         input.appendChild(option2);
 
         const option3 = document.createElement("option");
         option3.value = 2;
-        option3.text = "Normal";
+        option3.text = "Deafen only";
         input.appendChild(option3);
+
+        const option4 = document.createElement("option");
+        option4.value = 3;
+        option4.text = "Always open";
+        input.appendChild(option4);
+
+        const option5 = document.createElement("option");
+        option5.value = 4;
+        option5.text = "Normal";
+        input.appendChild(option5);
 
         input.addEventListener("change", () => {
             mySettings["mode"] = input.value;
@@ -96,7 +95,8 @@ module.exports = meta => {
         for (const options of input.options) {
             if (options.value == mySettings.mode) {
                 options.selected = "selected"
-            } else {
+            }
+            else {
                 options.selected = ""
             }
         }
@@ -107,7 +107,6 @@ module.exports = meta => {
     return {
         start: () => {
             Object.assign(mySettings, BdApi.loadData("FakeMuteDeafen", "settings"));
-            console.log(mySettings.mode)
             app_mode = mySettings.mode
             WebSocket.prototype.send = WebSocket.prototype.send_modded;
         },
@@ -124,5 +123,4 @@ module.exports = meta => {
             return mySettingsPanel;
         }
     }
-
 }
